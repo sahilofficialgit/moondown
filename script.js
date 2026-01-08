a9fc4186020ba3461df4b4fde0c4032c901a85cd
shaikh-tech-gh <sahilshaikh0729@gmail.com>
2026-01-08 16:49:53 +0530

    Added cursor on All pages

--- script.js
+++ script.js
@@ -4,9 +4,54 @@ const scroll = new LocomotiveScroll({
 });
 
 var page1 = document.querySelector(".page1")
+var page2 = document.querySelector(".page2")
+var page3 = document.querySelector(".page3")
+var page4 = document.querySelector(".page4")
+var page5 = document.querySelector(".page5")
+var page6 = document.querySelector(".page6")
 var cursor = document.querySelector(".cursor")
 
 page1.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+        x:dets.x,
+        y:dets.y,
+        duration: 0.6
+    })
+})
+
+page2.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+        x:dets.x,
+        y:dets.y,
+        duration: 0.6
+    })
+})
+
+page3.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+        x:dets.x,
+        y:dets.y,
+        duration: 0.6
+    })
+})
+
+page4.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+        x:dets.x,
+        y:dets.y,
+        duration: 0.6
+    })
+})
+
+page5.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+        x:dets.x,
+        y:dets.y,
+        duration: 0.6
+    })
+})
+
+page6.addEventListener("mousemove",function(dets){
     gsap.to(".cursor",{
         x:dets.x,
         y:dets.y,
